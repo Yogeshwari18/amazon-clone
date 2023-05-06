63228f44671b7d9d02ef0f02f02208a8cea68ca7
Yogeshwari18 <yogikumawat09@gmail.com>
2023-05-07 00:11:46 +0530

    Updates

---
+++ precache-manifest.0595b2ff7d506b5c5594d14f7aebeb64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de11fe388838f90ab60de6e9ad2713e",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "20cabccef5d9afb8d822",
+    "url": "/amazon-clone/static/css/main.b3588f7e.chunk.css"
+  },
+  {
+    "revision": "308365cd669e77f2a7e2",
+    "url": "/amazon-clone/static/js/2.328bb88c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.328bb88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cabccef5d9afb8d822",
+    "url": "/amazon-clone/static/js/main.5023a443.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
